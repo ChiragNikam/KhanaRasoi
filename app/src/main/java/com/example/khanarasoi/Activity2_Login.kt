ad5d908acf43e86056710d41505c77ffd6872828
Chirag Nikam <chiragnikam02@gmail.com>
2023-07-31 11:08:20 +0530

    layout - completed sign up activity layout with username, email and password

--- app/src/main/java/com/example/khanarasoi/Activity2_Login.kt
+++ app/src/main/java/com/example/khanarasoi/Activity2_Login.kt
@@ -1,11 +1,21 @@
 package com.example.khanarasoi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.khanarasoi.databinding.Activity1AppInfoBinding
+import com.example.khanarasoi.databinding.Activity2LoginBinding
 
 class Activity2_Login : AppCompatActivity() {
+    private val binding: Activity2LoginBinding by lazy {
+        Activity2LoginBinding.inflate(layoutInflater)
+    };
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity2_login)
+        setContentView(binding.root)
+        binding.txtViewDontHaveAccount.setOnClickListener{
+            val intent = Intent(this, Activity3_SignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/khanarasoi/Activity3_SignUp.kt
@@ -0,0 +1,20 @@
+package com.example.khanarasoi
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.khanarasoi.databinding.Activity3SignUpBinding
+
+class Activity3_SignUp : AppCompatActivity() {
+    private val binding : Activity3SignUpBinding by lazy {
+        Activity3SignUpBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.txtViewHaveAccount.setOnClickListener{
+            val intent = Intent(this, Activity2_Login::class.java)
+            startActivity(intent)
+        }
+    }
+}
