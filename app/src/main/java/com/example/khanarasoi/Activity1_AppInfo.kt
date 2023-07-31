6157dd97ca72937ffde03f5911357374ba73c6a4
Chirag Nikam <chiragnikam02@gmail.com>
2023-07-31 08:36:20 +0530

    feature - added view_binding in gradle script

--- app/src/main/java/com/example/khanarasoi/Activity1_AppInfo.kt
+++ app/src/main/java/com/example/khanarasoi/Activity1_AppInfo.kt
@@ -4,13 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.khanarasoi.databinding.Activity1AppInfoBinding
 
 class Activity1_AppInfo : AppCompatActivity() {
+    private val binding: Activity1AppInfoBinding by lazy {
+        Activity1AppInfoBinding.inflate(layoutInflater)
+    };
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity1_main_activity)
-        val button = findViewById<Button>(R.id.button_next)
-        button.setOnClickListener {
+        setContentView(binding.root)
+        binding.buttonNext.setOnClickListener {
             val intent  = Intent(this, Activity2_Login::class.java)
             startActivity(intent)
         }
