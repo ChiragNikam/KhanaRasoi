53352173336c163b8d7f6c657762ed14b547af7b
Chirag Nikam <chiragnikam02@gmail.com>
2023-08-16 13:53:05 +0530

    layout - completed layout of menu items into bottom sheet dialog fragment

--- app/src/main/java/com/example/khanarasoi/a_Fragments/Fragment1_1_ViewMenu.kt
+++ app/src/main/java/com/example/khanarasoi/a_Fragments/Fragment1_1_ViewMenu.kt
@@ -1,64 +1,40 @@
 package com.example.khanarasoi.a_Fragments
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.denzcoskun.imageslider.constants.ScaleTypes
-import com.denzcoskun.imageslider.models.SlideModel
+import com.example.khanarasoi.R
 import com.example.khanarasoi.b_Adapter.Adapter1_FoodItems
 import com.example.khanarasoi.c_Class.Class0_FoodItemsInfo
-import com.example.khanarasoi.databinding.Fragment1HomeBinding
+import com.example.khanarasoi.databinding.Fragment11ViewMenuBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class Fragment1_Home : Fragment() {
-    private val binding: Fragment1HomeBinding by lazy {
-        Fragment1HomeBinding.inflate(layoutInflater)
-    }
+class Fragment1_1_ViewMenu : BottomSheetDialogFragment() {
+
+    private lateinit var
+            binding: Fragment11ViewMenuBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding.btnViewMenu.setOnClickListener {
-            val menu = Fragment1_1_ViewMenu()
-            menu.show(parentFragmentManager, "menu")
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Inflate the layout for this fragment
+        binding = Fragment11ViewMenuBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Add image to Array List (of type SlideModel) by image links
-        val imageList = ArrayList<SlideModel>()
-        imageList.add(
-            SlideModel(
-                "https://img.freepik.com/free-psd/food-menu-restaurant-web-banner-template_120329-1771.jpg?size=626&ext=jpg",
-                ScaleTypes.FIT
-            )
-        )
-        imageList.add(
-            SlideModel(
-                "https://img.freepik.com/free-psd/food-menu-restaurant-facebook-cover-template_106176-731.jpg?size=626&ext=jpg",
-                ScaleTypes.FIT
-            )
-        )
-        imageList.add(
-            SlideModel(
-                "https://img.freepik.com/free-psd/food-menu-restaurant-web-banner-template_106176-422.jpg?size=626&ext=jpg",
-                ScaleTypes.FIT
-            )
-        )
-
-        // Set images of array list into image slider
-        binding.imageSlider.setImageList(imageList)
 
         // Set food items with there price, food name , food image url
         val food1 = Class0_FoodItemsInfo("Vada Pao", 10, "https://img.freepik.com/free-photo/traditional-indian-soup-lentils-indian-dhal-spicy-curry-bowl-spices-herbs-rustic-black-wooden-table_2829-18717.jpg?size=626&ext=jpg")
@@ -67,9 +43,13 @@ class Fragment1_Home : Fragment() {
         val food4 = Class0_FoodItemsInfo("Burgeer", 10, "https://img.freepik.com/free-photo/front-view-burger-stand_141793-15542.jpg?size=626&ext=jpg")
         val food5 = Class0_FoodItemsInfo("Vada Pao", 10, "https://img.freepik.com/free-photo/traditional-indian-soup-lentils-indian-dhal-spicy-curry-bowl-spices-herbs-rustic-black-wooden-table_2829-18717.jpg?size=626&ext=jpg")
         val food6 = Class0_FoodItemsInfo("Pav Bhaji", 20, "https://images.lifestyleasia.com/wp-content/uploads/sites/7/2022/02/01171421/YFL-Pav-Bhaji-2.jpg?tr=w-1600")
-        val food_details = mutableListOf(food1, food2, food3, food4, food5, food6)
+        val food7 = Class0_FoodItemsInfo("Pav Bhaji", 20, "https://images.lifestyleasia.com/wp-content/uploads/sites/7/2022/02/01171421/YFL-Pav-Bhaji-2.jpg?tr=w-1600")
+        val food8 = Class0_FoodItemsInfo("Pav Bhaji", 20, "https://images.lifestyleasia.com/wp-content/uploads/sites/7/2022/02/01171421/YFL-Pav-Bhaji-2.jpg?tr=w-1600")
+        val food9 = Class0_FoodItemsInfo("Pav Bhaji", 20, "https://images.lifestyleasia.com/wp-content/uploads/sites/7/2022/02/01171421/YFL-Pav-Bhaji-2.jpg?tr=w-1600")
+        val food10 = Class0_FoodItemsInfo("Pav Bhaji", 20, "https://images.lifestyleasia.com/wp-content/uploads/sites/7/2022/02/01171421/YFL-Pav-Bhaji-2.jpg?tr=w-1600")
+        val food_details = mutableListOf(food1, food2, food3, food4, food5, food6, food7, food8, food9, food10)
         val adapter = Adapter1_FoodItems(food_details)
-        binding.recyclerFoodItems.layoutManager = LinearLayoutManager(requireContext())
-        binding.recyclerFoodItems.adapter = adapter
+        binding.recyclerMenu.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerMenu.adapter = adapter
     }
 }
