1c848f3dec38d20a039763bd6833e4475453a81c
Chirag Nikam <chiragnikam02@gmail.com>
2023-08-31 23:52:37 +0530

    layout - improvement in recycler view, it will show an empty view after the last view in the recycler view, So that user can see last view properly which was hiding behind the button

--- app/src/main/java/com/example/khanarasoi/b_Adapter/Adapter2_CartItems.kt
+++ app/src/main/java/com/example/khanarasoi/b_Adapter/Adapter2_CartItems.kt
@@ -5,56 +5,91 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.khanarasoi.c_Class.Class1_CartItemsInfo
 import com.example.khanarasoi.databinding.Layout1CartItemsBinding
+import com.example.khanarasoi.databinding.Layout3CartEmptyItemBinding
 import com.squareup.picasso.Picasso
 
 class Adapter2_CartItems(private val cartItems: MutableList<Class1_CartItemsInfo>) :
-    RecyclerView.Adapter<Adapter2_CartItems.Adapter_Binding>() {
-    // bug here
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Adapter_Binding {
-        return Adapter_Binding(
-            Layout1CartItemsBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-        )
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val VIEW_TYPE_CART_ITEM = 0
+    private val VIEW_TYPE_EMPTY = 1
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_CART_ITEM -> {
+                val binding = Layout1CartItemsBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                CartItemViewHolder(binding)
+            }
+            VIEW_TYPE_EMPTY -> {
+                val binding = Layout3CartEmptyItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                EmptyViewHolder(binding)
+            }
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
     }
 
-    override fun onBindViewHolder(holder: Adapter_Binding, position: Int) {
-        val cart_item = cartItems[position]
-        holder.binding.txtViewFoodName.text = cart_item.food_name
-        holder.binding.txtViewFoodPrice.text = buildString {
-            append("\u20B9")
-            append(cart_item.food_price.toString())
-        }
-        holder.binding.txtViewItemCount.text = cart_item.item_count.toString()
-        Picasso.get().load(cart_item.food_logo).into(holder.binding.imgCartFoodItem)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is CartItemViewHolder -> {
+                val cart_item = cartItems[position]
+                holder.binding.txtViewFoodName.text = cart_item.food_name
+                holder.binding.txtViewFoodPrice.text = buildString {
+                    append("\u20B9")
+                    append(cart_item.food_price.toString())
+                }
+                holder.binding.txtViewItemCount.text = cart_item.item_count.toString()
+                Picasso.get().load(cart_item.food_logo).into(holder.binding.imgCartFoodItem)
 
-        holder.binding.btnPlus.setOnClickListener {
-            var item_count: Int = holder.binding.txtViewItemCount.text.toString().toInt()
-            if (item_count < 10) {
-                item_count++
-                holder.binding.txtViewItemCount.text = item_count.toString()
+                holder.binding.btnPlus.setOnClickListener {
+                    var item_count: Int = holder.binding.txtViewItemCount.text.toString().toInt()
+                    if (item_count < 10) {
+                        item_count++
+                        holder.binding.txtViewItemCount.text = item_count.toString()
+                    }
+                }
+
+                holder.binding.btnMunus.setOnClickListener {
+                    var item_count: Int = holder.binding.txtViewItemCount.text.toString().toInt()
+                    if (item_count > 1) {
+                        item_count--
+                        holder.binding.txtViewItemCount.text = item_count.toString()
+                    } else {
+                        deleteCartItem(position)
+                    }
+                }
+
+                holder.binding.btnDelete.setOnClickListener {
+                    deleteCartItem(position)
+                }
             }
-        }
-
-        holder.binding.btnMunus.setOnClickListener {
-            var item_count: Int = holder.binding.txtViewItemCount.text.toString().toInt()
-            if (item_count > 1) {
-                item_count--
-                holder.binding.txtViewItemCount.text = item_count.toString()
-            } else {
-                deleteCartItem(position)
+            is EmptyViewHolder -> {
+                // No operation needed for the empty view
             }
-        }
-
-        holder.binding.btnDelete.setOnClickListener {
-            deleteCartItem(position)
         }
     }
 
     override fun getItemCount(): Int {
-        return cartItems.size
+        return if (cartItems.isEmpty()) {
+            0
+        } else {
+            cartItems.size + 1
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position < cartItems.size) {
+            VIEW_TYPE_CART_ITEM
+        } else {
+            VIEW_TYPE_EMPTY
+        }
     }
 
     private fun deleteCartItem(position: Int) {
@@ -63,7 +98,10 @@ class Adapter2_CartItems(private val cartItems: MutableList<Class1_CartItemsInfo
         notifyItemRangeChanged(position, cartItems.size)
     }
 
-    class Adapter_Binding(val binding: Layout1CartItemsBinding) :
+    inner class CartItemViewHolder(val binding: Layout1CartItemsBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    inner class EmptyViewHolder(val binding: Layout3CartEmptyItemBinding) :
         RecyclerView.ViewHolder(binding.root)
 
 }
